708009bdd920a9c8575a2eb2a995fe64833dce61
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-12-10 13:14:22 +0800

    landing page

--- my-app/app/start-page/page.tsx
+++ my-app/app/start-page/page.tsx
@@ -1,15 +1,25 @@
+"use client";
+
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function StartPage() {
-    return (
-        <div style={{position: 'relative', width: '100%', height: '100vh'}}>
-             <Image
-                src="/1.png"
-                layout="fill"
-                objectFit="cover"
-                alt="background pic"
-                />
+  return (
+    <div className="relative w-full h-screen">
+      <Image
+        src="/1.png"
+        alt="background pic"
+        fill={true}
+        object-fit="cover"
+        className="-z-50"
+      />
+      <div className="flex items-center justify-center h-screen">
+        <div className="text-center text-white p-6 flex gap-4">
+          <Button className="bg-transparent text-4xl font-bold border border-white rounded-lg px-8 py-8 transition-transform hover:bg-transparent hover:scale-105">
+            Descend into Madness →
+          </Button>
         </div>
-       
-    )
+      </div>
+    </div>
+  );
 }
