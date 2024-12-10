c336104abb5de65061f50907a5adcce20f6dd4fd
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-12-10 13:54:40 +0800

    added gradient for btn, link to quiz page

--- my-app/app/start-page/page.tsx
+++ my-app/app/start-page/page.tsx
@@ -1,23 +1,32 @@
 "use client";
 
+import { BackgroundGradient } from "@/components/ui/background-gradient";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function StartPage() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/quiz");
+  };
+
   return (
     <div className="relative w-full h-screen">
       <Image
         src="/1.png"
         alt="background pic"
         fill={true}
-        object-fit="cover"
+        objectFit="cover"
         className="-z-50"
       />
       <div className="flex items-center justify-center h-screen">
-        <div className="text-center text-white p-6 flex gap-4">
-          <Button className="bg-transparent text-4xl font-bold border border-white rounded-lg px-8 py-8 transition-transform hover:bg-transparent hover:scale-105">
-            Descend into Madness →
-          </Button>
+        <div className="text-center p-6 flex gap-4 hover:scale-105 transition-transform">
+          <BackgroundGradient className="rounded-[22px] bg-white dark:bg-zinc-900 bg-opacity-90">
+            <button className="bg-transparent text-4xl font-bold px-6 py-4" onClick={handleClick}>
+              Descend into Madness →
+            </button>
+          </BackgroundGradient>
         </div>
       </div>
     </div>
