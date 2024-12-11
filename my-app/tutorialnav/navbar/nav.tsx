248dba769bb4e2906a2f6793c459c3a68974799f
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-12-11 10:44:26 +0800

    fix build

--- my-app/tutorialnav/navbar/nav.tsx
+++ my-app/tutorialnav/navbar/nav.tsx
@@ -3,15 +3,10 @@
 import * as React from "react"
 import Link from "next/link"
 
-import { cn } from "@/lib/utils"
-import { Icons } from "@/components/icons"
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 
