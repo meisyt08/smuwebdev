fb6385b75aa3d9bf78717de2fbbadfc2c01ba563
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-12-11 15:57:47 +0800

    removed dropdown from nav, format quiz

--- my-app/app/quiz/page.tsx
+++ my-app/app/quiz/page.tsx
@@ -13,25 +13,30 @@ export default function Quiz() {
   };
 
   return (
-    <div className="px-4">
-      <Image
-        src="/1.png"
-        alt="background pic"
-        fill={true}
-        className="-z-50 object-cover pointer-events-none"
-      />
-      <h1>Interactive Game</h1>
-      <p>{currentState.text}</p>
-      <div>
-        {Object.keys(currentState.options).map((optionKey) => (
-          <button
-            key={optionKey}
-            onClick={() => handleOptionClick(currentState.options[optionKey])}
-            style={{ margin: "5px" }}
-          >
-            {optionKey}
-          </button>
-        ))}
+    <div className="w-screen h-[90vh] flex justify-center items-center">
+      <div className="px-4">
+        <Image
+          src="/1.png"
+          alt="background pic"
+          fill={true}
+          className="-z-50 object-cover pointer-events-none"
+        />
+        <div className="text-white text-center">
+          <h1 className="mb-4 text-2xl max-w-3xl">{currentState.text}</h1>
+          <div className="text-3xl">
+            {Object.keys(currentState.options).map((optionKey) => (
+              <button
+                key={optionKey}
+                onClick={() =>
+                  handleOptionClick(currentState.options[optionKey])
+                }
+                className="mt-2 px-6"
+              >
+                {optionKey}
+              </button>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
