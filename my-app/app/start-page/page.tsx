30e8433166627476417121862d9fbaa916460568
unknown <Javent2003@gmail.com>
2024-12-12 10:41:49 +0800

    start page

--- my-app/app/start-page/page.tsx
+++ my-app/app/start-page/page.tsx
@@ -38,6 +38,7 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import React from 'react';
 
 export default function StartPage() {
   const router = useRouter();
@@ -46,8 +47,15 @@ export default function StartPage() {
     router.push("/quiz");
   };
 
+  React.useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'auto'; // Revert back when the component unmounts
+    };
+  }, []);
+
   return (
-    <div className="w-full h-screen relative group">
+    <div className="w-full h-screen group fixed-top" >
       {/* Background image with blur effect on hover */}
       <Image
         src="/The sun.jpg"
@@ -58,18 +66,10 @@ export default function StartPage() {
       {/* Content area */}
       <div className="flex items-center justify-center h-screen">
         <div className="text-center p-6 flex gap-4">
-          {/* Button to trigger the hover effect */}
-          {/* <button
-            className="bg-transparent text-4xl font-bold text-white px-5 hover:scale-105 hover:pr-16 hover:backdrop-blur-sm transition-all"
-            onClick={handleClick}
-            aria-label="Start the quiz"
-          >
-            Descend into Madness →
-          </button> */}
           <img
             src="/new sun.png"  // Replace with the path to your image
             alt="Descend into Madness"
-            className="cursor-pointer hover:scale-106 transition-all w-[550px] h-[596px]"
+            className="cursor-pointer hover:scale-106 transition-all w-[550px] h-[596px] mt-[-120px]"
             onClick={handleClick}
           />
         </div>
