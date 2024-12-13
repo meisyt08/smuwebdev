4503d9a0c3cb71db640ac4d78bf0c17e460103b9
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-12-13 13:12:22 +0800

    stops audio on other pages

--- my-app/app/quiz/page.tsx
+++ my-app/app/quiz/page.tsx
@@ -1,12 +1,28 @@
 "use client";
 
 import Quiz from "@/components/quiz";
+import { useEffect, useRef } from "react";
 
 export default function QuizPage() {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+
+    return () => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+        audioRef.current.currentTime = 0;
+      }
+    };
+  }, []);
+
   return (
     <main className="flex items-center justify-center p-24 mt-20">
       <Quiz />
-      <audio autoPlay loop>
+      <audio ref={audioRef} autoPlay loop>
         <source src="/groves-small.mp3" type="audio/mpeg" />
         Your browser does not support the audio element.
       </audio>
