199a2d1036c50d59355c35b8f82a627eddca4401
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2024-12-13 13:08:21 +0800

    add audio

--- my-app/app/quiz/page.tsx
+++ my-app/app/quiz/page.tsx
@@ -6,6 +6,10 @@ export default function QuizPage() {
   return (
     <main className="flex items-center justify-center p-24 mt-20">
       <Quiz />
+      <audio autoPlay loop>
+        <source src="/groves-small.mp3" type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
     </main>
   );
 }
